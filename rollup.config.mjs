cdd9023063ec73d9eeb6b32ee329a36ef917c7f0
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-08-29 17:35:57 -0400

    configuración para usar es6

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,21 +1,57 @@
+import path from 'path'
 import { defineConfig } from 'rollup'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import { babel } from '@rollup/plugin-babel'
+import { getBabelOutputPlugin } from '@rollup/plugin-babel'
+
+const getOutputFileName = (path, name, infoExtension) => {
+  return `${path}/${name}.${infoExtension.extension}`
+}
+
+// Extension Information
+const CJS = {
+  extension: 'js',
+  format: 'cjs',
+}
+const MJS = {
+  extension: 'mjs',
+  format: 'es',
+}
+
+const INPUT_FILE = 'src/index.js'
+
+// Unminified transpiler files
+const OUTPUT_DIR = 'dist'
+const OUTPUT_CJS_FILE = getOutputFileName(OUTPUT_DIR, 'index', CJS)
+const OUTPUT_MJS_FILE = getOutputFileName(OUTPUT_DIR, 'index', MJS)
+
+const BABEL_CONFIG_FILE = path.resolve('.', 'babel.config.json')
 
 export default defineConfig({
-  input: 'src/index.js',
+  input: INPUT_FILE,
   output: [
     {
-      file: 'dist/index.js',
-      format: 'cjs',
+      file: OUTPUT_CJS_FILE,
+      format: CJS.format,
       sourcemap: true,
+      plugins: [
+        getBabelOutputPlugin({
+          configFile: BABEL_CONFIG_FILE,
+          envName: 'es5',
+        }),
+      ],
     },
     {
-      file: 'dist/index.mjs',
-      format: 'es',
+      file: OUTPUT_MJS_FILE,
+      format: MJS.format,
       sourcemap: true,
+      plugins: [
+        getBabelOutputPlugin({
+          configFile: BABEL_CONFIG_FILE,
+          envName: 'es6',
+        }),
+      ],
     },
   ],
   external: [/node_module/],
-  plugins: [nodeResolve(), babel({ babelHelpers: 'bundled' })],
+  plugins: [nodeResolve()],
 })
