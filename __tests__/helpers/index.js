43050e82d3ad88eeeb59d3a4b7a646248f0ae56d
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-10-18 23:54:23 -0300

    refactor: se agregan helpers que se encontraban en archivos de test

--- __tests__/helpers/index.js
+++ __tests__/helpers/index.js
@@ -1,5 +1,19 @@
 import { faker } from '@faker-js/faker'
+import { ReactiveLocalStorage } from '../../src/reactiveLocalStorage/storage/ReactiveLocalStorage'
 
 faker.seed(129)
+
+export const getDefaultSerializer = () => ({
+  serialize: (...parameters) => JSON.stringify(...parameters),
+  parse: (...parameters) => JSON.parse(...parameters),
+})
+
+export const getReactiveLocalStorageInstance = (
+  reactiveStorage,
+  webStorage,
+  serializer,
+) => new ReactiveLocalStorage(reactiveStorage, webStorage, serializer)
+
+export const { localStorage } = window
 
 export { faker }
