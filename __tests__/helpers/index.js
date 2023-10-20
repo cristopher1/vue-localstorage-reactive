e319c92c5018fd72a068f0bba3f489cf7bfbeb30
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-10-20 16:23:46 -0300

    test: agregado nuevos helpers

--- __tests__/helpers/index.js
+++ __tests__/helpers/index.js
@@ -14,6 +14,32 @@ export const getReactiveLocalStorageInstance = (
   serializer,
 ) => new ReactiveLocalStorage(reactiveStorage, webStorage, serializer)
 
+export const createArrayTestWithObjects = (nElement) => {
+  const array = []
+  for (let i = 0; i < nElement; i++) {
+    const element = {
+      key: faker.string.sample(),
+      value: {
+        string: faker.string.sample(),
+        number: faker.number.int(),
+        array: new Array(faker.number.int({ max: 100 })),
+      },
+    }
+    array.push(element)
+  }
+  return array
+}
+
+export const addItemsInReactiveLocalStorage = (
+  elements,
+  reactiveLocalStorage,
+) => {
+  elements.forEach((element) => {
+    const { key, value } = element
+    reactiveLocalStorage.setItem(key, value)
+  })
+}
+
 export const { localStorage } = window
 
 export { faker }
