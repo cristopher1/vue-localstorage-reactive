4fcf44338f987940cd62f194217f5a3d88fd1333
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2023-10-22 15:39:01 -0300

    refactor(installer): ya no se usan listeners para eventos storage

--- src/reactiveLocalStorage/installer/ReactiveLocalStorageInstaller.js
+++ src/reactiveLocalStorage/installer/ReactiveLocalStorageInstaller.js
@@ -1,8 +1,6 @@
 import { LoadDataFromLocalStorageFactory } from '../listeners/LoadDataFromLocalStorageFactory'
-import { RemoveItemFromLocalStorageFactory } from '../listeners/RemoveItemFromLocalStorageFactory'
-import { SetItemFromLocalStorageFactory } from '../listeners/SetItemFromLocalStorageFactory'
 import { ReactiveLocalStorageBuilder } from '../builder/ReactiveLocalStorageBuilder'
-import { reactive } from 'vue'
+import { App, reactive } from 'vue'
 
 export class ReactiveLocalStorageInstaller {
   #activateLoadDataFromLocalStorageEventListener(reactiveLocalStorage) {
@@ -11,35 +9,27 @@ export class ReactiveLocalStorageInstaller {
     window.addEventListener('load', onLoad)
   }
 
-  #activateRemoveItemFromLocalStorageListener(reactiveLocalStorage) {
-    const onRemove =
-      RemoveItemFromLocalStorageFactory.createListener(reactiveLocalStorage)
-    window.addEventListener('storage', onRemove)
-  }
-
-  #activateSetItemFromLocalStorageListener(reactiveLocalStorage) {
-    const onAdd =
-      SetItemFromLocalStorageFactory.createListener(reactiveLocalStorage)
-    window.addEventListener('storage', onAdd)
-  }
-
   #addReactiveLocalStorageToApp(app, reactiveLocalStorage) {
-    app.config.globalProperties.$reactiveWebStorage = {
-      localStorage: reactiveLocalStorage,
-    }
+    app.config.globalProperties.$reactiveLocalStorage = reactiveLocalStorage
   }
 
   /**
-   * @param {App<Element>} app - Instance of createApp
-   * @param {object} options - Configuration of this plugin
+   * Installs the plugin.
+   *
+   * @param {App<Element>} app Instance of the app created by createApp.
+   * @param {object} options Plugin's configuration.
+   * @param {boolean} [options.useRefStorage] Defines the reactive storage used.
+   *   When this parameter is false, it is used a reactive object to save the
+   *   data; otherwise, it is used a ref object. By default is true.
+   * @param {object} [options.serializer] An object used to serializes and
+   *   unserializes data. By default is used a wrapped JSON.
+   * @param {Function} options.serializer.serialize A method used to serialize
+   *   data.
+   * @param {Function} options.serializer.parse A method used to unserialize
+   *   data.
    */
   install(app, options = {}) {
-    const {
-      useRefStorage = true,
-      useRemoveItemFromLocalStorage = false,
-      useAddItemFromLocalStorage = false,
-      serializer = undefined,
-    } = options
+    const { useRefStorage = true, serializer = undefined } = options
 
     const reactiveLocalStorageBuilder = new ReactiveLocalStorageBuilder()
 
@@ -54,15 +44,6 @@ export class ReactiveLocalStorageInstaller {
     const reactiveLocalStorage = reactiveLocalStorageBuilder.build()
 
     this.#activateLoadDataFromLocalStorageEventListener(reactiveLocalStorage)
-
-    if (useRemoveItemFromLocalStorage) {
-      this.#activateRemoveItemFromLocalStorageListener(reactiveLocalStorage)
-    }
-
-    if (useAddItemFromLocalStorage) {
-      this.#activateSetItemFromLocalStorageListener(reactiveLocalStorage)
-    }
-
     this.#addReactiveLocalStorageToApp(app, reactiveLocalStorage)
   }
 }
